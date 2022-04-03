c0e353aca2dc9b87b51b84c50b7ee868b30c4e2a
lev <levostatnigrosh@gmail.com>
2022-04-03 19:47:34 -0400

    issue #4 added bundle messenger

--- app/app.py
+++ app/app.py
@@ -1,21 +1,28 @@
 import os
+import json
+from typing import List
 from twilio.rest import Client
-from dotenv import load_dotenv
 
-from app.crud import Message, format_data
+from dotenv import load_dotenv
+from app.crud import Message
 
 load_dotenv()
 
-sender = os.getenv("SENDER")
+senders = os.getenv("SENDERS")
 twilio_number = os.getenv("TWILIO_NUMBER")
 account_sid = os.getenv("TWILIO_ACCOUNT_SID")
 auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+notify_service_sid = os.getenv("TWILIO_NOTIFY_SERVICE_SID")
 client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-    body=Message.from_current().create_message(),
-    from_=twilio_number,
-    to=sender,
-)
 
-print(message.sid)
+def send_bulk_sms():
+    for number in senders.split(","):
+        client.messages.create(
+            body=Message.from_current().create_message(),
+            from_=twilio_number,
+            to=number,
+        )
+
+
+send_bulk_sms()
