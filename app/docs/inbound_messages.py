a9efb165d4d6a7152d7e988fc817f286540c18d9
lev <levostatnigrosh@gmail.com>
2022-04-06 13:07:26 -0400

    refactored inbound messages and the test

--- app/docs/inbound_messages.py
+++ app/docs/inbound_messages.py
@@ -1,29 +1,26 @@
 import os
-from typing import List
+from typing import Any, List
+
 from geotext import GeoText
 
 from .config_twilio import Twilio
 
 
-def inbound_location(messages: List) -> str:
+def inbound_location(messages: List[Any]) -> str:
     """
-    Return the location of an inbound message.
+    This function takes in a list of messages and returns the location of the
+    first message that contains a location.
 
-    loop through the inbound messages and return the city name.
-    If the location is not found, return set default location.
+    :param messages: A list of messages.
+
+    :return: The location of the inbound message.
+    if no location is found, default to location.
     """
 
     default_location = os.getenv("DEFAULT_LOCATION")
     twilio_number = Twilio.TWILIO_NUMBER
-
-    try:
-        for key, sms in enumerate(messages):
-            if sms.to == twilio_number:
-                inbound_city = GeoText(sms.body).cities[0]
-                break
-            else:
-                inbound_city = default_location
-
-    except TypeError:
-        raise TypeError("list was not iterable")
+    last_message = max(messages, key=lambda x: x == twilio_number)
+    inbound_city = GeoText(last_message.body).cities[0]
+    if inbound_city is None:
+        inbound_city = default_location
     return inbound_city
--- app/tests/docs/test_inbound_messages.py
+++ app/tests/docs/test_inbound_messages.py
@@ -1,7 +1,9 @@
 import pytest
+
 from app.docs import inbound_location
 
 
 def test_inbound_location():
     with pytest.raises(Exception):
         inbound_location(None)
+        inbound_location(3)
