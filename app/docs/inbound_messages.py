7c05be9015bdc0d2744bc5e50bb97f61489245d5
lev <levostatnigrosh@gmail.com>
2022-04-12 19:38:01 -0400

    final max and min update

--- app/docs/inbound_messages.py
+++ app/docs/inbound_messages.py
@@ -1,6 +1,7 @@
 import os
 from geopy.geocoders import Photon
 import geocoder
+
 from typing import Any, List
 
 from geotext import GeoText
@@ -18,9 +19,23 @@ def inbound_location(messages: List[Any]) -> str:
     :return: The location of the inbound message.
     if no location is found, default to location.
     """
-
     twilio_number = Twilio.TWILIO_NUMBER
-    last_message = max(messages, key=lambda x: x == twilio_number)
+
+    # incoming_numbers = set(
+    #     [message.from_ for message in messages if message.from_ != twilio_number]
+    # )
+    # user_messages: list = []
+    # for number in incoming_numbers:
+    #     last_message = max(messages, key=lambda x: x.from_ == number)
+    #     location = GeoText(last_message.body)
+    #     if location.cities:
+    #         user = {"number": number, "location": location.cities[0]}
+    #         user_messages.append(user)
+    # last_messages["number"] = number
+    # last_messages["location"] = location.cities[0]
+    # return user_messages
+    last_message = max(messages, key=lambda x: x.to == twilio_number)
+    # number = last_message.from_
     inbound_city = GeoText(last_message.body).cities[0]
     if inbound_city is None:
         inbound_city = os.environ["DEFAULT_LOCATION"]
@@ -37,11 +52,11 @@ def coordinates(messages: List[Any]) -> str:
     """
 
     twilio_number = Twilio.TWILIO_NUMBER
-    last_message = max(messages, key=lambda x: x == twilio_number)
+    last_message = max(messages, key=lambda x: x.to == twilio_number)
     coordinates = geocoder.osm(last_message.body)
     if coordinates is None:
         coordinates = os.environ["DEFAULT_COORDINATES"]
     return coordinates.latlng
 
 
-# print(coordinates(Twilio.CLIENT.messages.list()))
+# print(inbound_location(Twilio.CLIENT.messages.list()))
