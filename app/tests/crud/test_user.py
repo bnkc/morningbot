4937151e988ebb517efc4167f75b3c04b4b340c7
lev <levostatnigrosh@gmail.com>
2022-05-04 18:26:52 -0400

    test

--- app/tests/crud/test_user.py
+++ app/tests/crud/test_user.py
@@ -23,29 +23,29 @@ def test_get_city(msg):
     assert user.get_city(invalid_msg) is None
 
 
-# def test_get_coords(msg):
-#     valid_msg, invalid_msg = msg
-#     assert user.get_coords(valid_msg) == [35.9603948, -83.9210261]
-#     assert user.get_coords(invalid_msg) is None
+def test_get_coords(msg):
+    valid_msg, invalid_msg = msg
+    assert user.get_coords(valid_msg) == [35.9603948, -83.9210261]
+    assert user.get_coords(invalid_msg) is None
 
 
-# def test_is_body_valid(msg):
-#     valid_msg, invalid_msg = msg
-#     assert user.is_body_valid(valid_msg) is True
-#     assert user.is_body_valid(invalid_msg) is False
+def test_is_body_valid(msg):
+    valid_msg, invalid_msg = msg
+    assert user.is_body_valid(valid_msg) is True
+    assert user.is_body_valid(invalid_msg) is False
 
 
-# def test_is_first_time_user(first_time_user):
-#     assert user.is_first_time_user(first_time_user[0]) is True
+def test_is_first_time_user(first_time_user):
+    assert user.is_first_time_user(first_time_user[0]) is True
 
 
-# def test_is_first_time_user_edge_case(added_user):
-#     assert user.is_first_time_user(added_user[0]) is False
+def test_is_first_time_user_edge_case(added_user):
+    assert user.is_first_time_user(added_user[0]) is False
 
 
-# def test_is_daily_limit_reached(added_user):
-#     assert user.is_daily_limit_reached(added_user[0]) is True
+def test_is_daily_limit_reached(added_user):
+    assert user.is_daily_limit_reached(added_user[0]) is True
 
 
-# def test_is_daily_limit_reached_edge_case(first_time_user):
-#     assert user.is_daily_limit_reached(first_time_user[0]) is False
+def test_is_daily_limit_reached_edge_case(first_time_user):
+    assert user.is_daily_limit_reached(first_time_user[0]) is False
